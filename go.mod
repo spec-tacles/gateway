118a3649b57fdfe1fd1fd2de8e89d71cd04e74a1
Will Nelson <will@wnelson.xyz>
2019-10-17 08:26:18 +0200

    feat(stats): begin adding stats recording

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/bwmarrin/snowflake v0.3.0 // indirect
 	github.com/gorilla/websocket v1.4.0
+	github.com/prometheus/client_golang v1.2.0
 	github.com/spec-tacles/go v0.0.0-20190703175512-7b05ffef1ab5
 	github.com/streadway/amqp v0.0.0-20190404075320-75d898a42a94 // indirect
 	github.com/stretchr/objx v0.2.0 // indirect
---
+++ stats/metrics.go
@@ -0,0 +1,48 @@
+package stats
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	// PacketsReceived is a counter of packets received
+	PacketsReceived = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "gateway",
+		Name: "packets_received",
+		Help: "Counter of packets received over all gateway connections.",
+	}, []string{"t", "op", "shard"})
+
+	// PacketsSent is a counter of packets sent
+	PacketsSent = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "gateway",
+		Name: "packets_sent",
+		Help: "Counter of packets sent over all gateway connections.",
+	}, []string{"t", "op", "shard"})
+
+	// ShardsAlive is a gauge of the number of shards alive
+	ShardsAlive = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "gateway",
+		Name: "shards_alive",
+		Help: "Number of shards that are online",
+	}, []string{"id"})
+
+	// TotalShards is a gauge of the total number of shards
+	TotalShards = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "gateway",
+		Name: "total_shards",
+		Help: "Total number of shards that should be online.",
+	})
+
+	// Ping is a summary of shard heartbeat latency
+	Ping = prometheus.NewSummaryVec(prometheus.SummaryOpts{
+		Namespace: "gateway",
+		Name: "ping",
+		Help: "Latency between heartbeat and acknowledgement (in milliseconds).",
+		Objectives: map[float64]float64{
+			0.5: 0.05,
+			0.9: 0.01,
+			0.95: 0.005,
+			0.99: 0.001,
+		},
+	}, []string{"id"})
+)
